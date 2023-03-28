bc6eee9f368027f31cf40313d4660f0ed1a805a8
Makoto3355 <mhototogisu83@gmail.com>
2023-03-28 01:31:40 -0700

    .

--- analysis/code/project_functions2.py
+++ analysis/code/project_functions2.py
@@ -14,16 +14,16 @@ def load_and_process(url_or_path_to_csv_file):
     )
     
     #Method Chain #2 adding columns and selecting necesarry columns
-df_4 = (pd.read_csv("../../Group14/data/raw/archive/matchesall2014-2020.csv")
-.dropna()
-.assign(possession_ratio=lambda x: x ['Home Team Possession %'] / x['Away Team Possession %'])   
-.assign(result_score_ratio=lambda x: x ['Home Team Goals Scored'] / x['Away Team Goals Scored'])
-.loc[:, ["Home Team", "Away Team", "Score", "Half Time Score", 'Match Excitement',
+    df_4 = (pd.read_csv("../../Group14/data/raw/archive/matchesall2014-2020.csv")
+    .dropna()
+    .assign(possession_ratio=lambda x: x ['Home Team Possession %'] / x['Away Team Possession %'])   
+    .assign(result_score_ratio=lambda x: x ['Home Team Goals Scored'] / x['Away Team Goals Scored'])
+    .loc[:, ["Home Team", "Away Team", "Score", "Half Time Score", 'Match Excitement',
                'Home Team Rating', 'Away Team Rating', 'Home Team Possession %', 'Away Team Possession %', 
                'Home Team Total Shots', 'Away Team Total Shots',  'Home Team Pass Success %','Away Team Pass Success %', 
                'Home Team Fouls','Home Team Yellow Cards', 'Home Team Second Yellow Cards','Home Team Red Cards',
                'Away Team Fouls', 'Away Team Yellow Cards', 'Away Team Second Yellow Cards', 'Away Team Red Cards',
                'Home Team Goals Scored', 'Away Team Goals Scored', 'year']]
-       )
+    )
     return df_4
 
